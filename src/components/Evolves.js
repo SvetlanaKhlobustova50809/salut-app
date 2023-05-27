96714799c61c0fa77ceebcd5ddee9b2775c26455
Svetlana Khlobustova <37548939+SvetlanaKhlobustova50809@users.noreply.github.com>
2023-05-27 23:24:57 +0300

    Update Evolves.js

--- src/components/Evolves.js
+++ src/components/Evolves.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { Button } from "@salutejs/plasma-ui";
-import { Link } from "react-router-dom";
-import { Navigate } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 
 export class Evolves extends React.Component {
   constructor(props) {
@@ -15,27 +14,33 @@ export class Evolves extends React.Component {
   render() {
     const { onEvolve } = this.props;
     const { onChangeEv } = this.props;
+    
+
     console.log("alloevolve", this.props.onChangeEv.notes[0].title);
     if (this.props.onChangeEv.notes[0].title != "no") {
-      return <Navigate to="/Unit" />;
+      return (
+      <Navigate to="/Unit" />
+      );
     }
 
     return (
       <div>
-        <h1 className="heading">Выбери свой Уровень</h1>
-
+        <h1 className="heading">Выбери свой уровень</h1>
+      <p>   </p>
         <div className="btn-group">
           {[1, 2, 3, 4, 5, 6].map((num) => (
-            <Link to="/Unit">
-              <Button key={num} onClick={() => this.props.onEvolve(num)}>
+              <Button
+                key={num}
+                onClick={() => this.props.onEvolve(num)}
+              >
                 {num}
               </Button>
-            </Link>
           ))}
         </div>
-        <div class="background">
-          <div class="transparent">Прозрачный текст</div>
-        </div>
+<div class="background">
+    <div class="transparent">Прозрачный текст</div>
+ <div class="transparent">Прозрачный текст</div>
+</div>
       </div>
     );
   }
