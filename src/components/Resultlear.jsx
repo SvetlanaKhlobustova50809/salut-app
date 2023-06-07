2093e0ab996a07506aa64047b2eee989eee9448d
Svetlana Khlobustova <37548939+SvetlanaKhlobustova50809@users.noreply.github.com>
2023-06-07 18:25:33 +0300

    Update Resultlear.jsx

--- src/components/Resultlear.jsx
+++ src/components/Resultlear.jsx
@@ -1,23 +1,27 @@
 import React from "react";
-import { useState } from "react";
-import { Button } from "@salutejs/plasma-ui";
-import { Link } from "react-router-dom";
+import {Button} from "@salutejs/plasma-ui";
+import {useNavigate} from "react-router-dom";
 
 
+function Resultlear() {
+  // function handleClick() {
+  //   window.location.href = '/';
+  // }
+  const navigate = useNavigate();
 
-function Resultlear()  {
-  function handleClick() {
-    window.location.href = '/';
-  }
-
-    return (
-      <div>
-        <h1 className="heading">Поздравляем, изучение пройдено!</h1>
-        <div className="btn-group1">
-          <Button onClick={() => handleClick()}>К началу</Button>
-        </div>
+  return (
+    <div>
+      <h1 className="heading">Поздравляем, изучение пройдено!</h1>
+      <div className="btn-group1">
+        {/*<Button onClick={() => handleClick()}>К началу</Button>*/}
+        <Button
+          onClick={() => navigate(`/`)}
+        >
+          К началу
+        </Button>
       </div>
-    );
+    </div>
+  );
 }
 
 export default Resultlear;
