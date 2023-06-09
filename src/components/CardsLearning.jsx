4968e9d17184034d9eedca9dd19d4479bae39eec
Svetlana Khlobustova <37548939+SvetlanaKhlobustova50809@users.noreply.github.com>
2023-06-09 18:23:25 +0300

    Update CardsLearning.jsx

--- src/components/CardsLearning.jsx
+++ src/components/CardsLearning.jsx
@@ -5,12 +5,9 @@ import ReactCardFlip from "react-card-flip";
 
 let data = require("./data.json");
 
-// console.log(data);
-
 export function get_data(evolve, unit) {
   const evolve_key = "evolve_" + evolve;
   const unit_key = "unit_" + unit;
-  // console.log(`get_data: evolve: ${evolve_key} unit: ${unit_key}`);
   const result = data[evolve_key][unit_key];
   let json_data = [];
   for (let i = 0; i < result.length; i += 2) {
@@ -23,141 +20,13 @@ export function get_data(evolve, unit) {
   return json_data;
 }
 
-// function CardsLearning(props) {
-//   const {evolve, unit, step: strStep, flip} = useParams();
-//   const {onOpen, onBack, onNext, onPrev, onFlip, onResult} = props;
-
-//   const step = parseInt(strStep);
-//   const repetitions = get_data(evolve, unit);
-//   const len = repetitions.length;
-//   const word = repetitions[step].title
-
-//   // const [flip, setFlip] = useState(false);
-
-//   useEffect(() => {
-//     // console.log("CardsLearning:: useEffect", {evolve, unit, step, word});
-//     onOpen({evolve, unit, step, flip, word});
-//     return () => {
-//     };
-//   }, [evolve, unit, step, flip, word]);
-
-//   const isLastWord = step >= len - 1;
-//   const isFirstWord = step <= 0;
-
-//   const Header = () => (
-//     <>
-//       <div className="btn-group1">
-//         <Button
-//           onClick={() => onBack({evolve, unit, step, flip, word})}
-//         >
-//           Назад
-//         </Button>
-//       </div>
-//       <br/>
-//       <div className="heading">
-//         <span>Уровень: {evolve} </span>
-//         <span>Раздел: {unit} </span>
-//       </div>
-//     </>
-//   )
-
-//   const TranslateButton = () => <Button
-//     // onClick={() => setFlip(!flip)}
-//     onClick={() => onFlip({evolve, unit, step, flip, word})}
-//   >
-//     Перевернуть карточку
-//   </Button>
-
-//   const FlipBackButton = () => <Button
-//     // onClick={() => setFlip(!flip)}
-//     onClick={() => onFlip({evolve, unit, step, flip, word})}
-//   >
-//     Перевернуть карточку
-//   </Button>
-
-//   const NextButton = () => {
-//     // console.log("CardsLearning: NextButton render", {evolve, unit, step, word});
-
-//     return <Button
-//       onClick={() => {
-
-//         // console.log("CardsLearning: NextButton onClick", {evolve, unit, step, word});
-
-//         onNext({evolve, unit, step, flip, word})
-//       }
-//       }
-//     >
-//       Следующее слово
-//     </Button>
-//   }
-
-//   const PrevButton = () => <Button
-//     onClick={() => onPrev({evolve, unit, step, flip, word})}
-//   >
-//     Предыдущее слово
-//   </Button>
-
-//   const ResultButton = () => <Button
-//     onClick={() => onResult({evolve, unit, step, flip, word})}
-//   >
-//     Результат
-//   </Button>
-
-//   const Group1 = () => (
-//     // <div className={`cardsOff ${showNextCard ? "hide" : ""}`}>
-//     <div className={`cardsOff`}>
-//       {word}
-//       <br/>
-//       <br/>
-//       <TranslateButton/>
-//       <br/>
-//       {isLastWord ? <ResultButton/> : <NextButton/>}
-//       <br/>
-//       {isFirstWord ? <></> : <PrevButton/>}
-//     </div>
-//   )
-
-//   const Group2 = () => (
-//     // <div className={`cardsOn ${showNextCard ? "show" : ""}`}>
-//     <div className={`cardsOn`}>
-//       {repetitions[step].correct}
-//       <br/>
-//       <br/>
-//       <FlipBackButton/>
-//       <br/>
-//       {isLastWord ? <ResultButton/> : <NextButton/>}
-//       <br/>
-//       {isFirstWord ? <></> : <PrevButton/>}
-//     </div>
-//   )
-
-//   // console.log("CardsLearning: render", {evolve, unit, step, word});
-
-//   return (
-//     <div>
-//       <Header/>
-
-//       <div className="btn-group2">
-//         <ReactCardFlip isFlipped={flip!=0} flipDirection="vertical">
-
-//           <Group1/>
-
-//           <Group2/>
-
-//         </ReactCardFlip>
-//       </div>
-//     </div>
-//   );
-// }
 
 function CardsLearning(props) {
   const { evolve, unit } = useParams();
-  const { onOpen, onBack, onNext, onPrev, onFlip, onResult } = props;
+  const { onOpen } = props;
 
-  // const step = parseInt(strStep);
   const repetitions = get_data(evolve, unit);
   const len = repetitions.length;
-  // const word = repetitions[step].title;
 
   useEffect(() => {
     onOpen({ evolve, unit});
@@ -167,23 +36,7 @@ function CardsLearning(props) {
   const navigate = useNavigate();
   function handleClick() {
     navigate(`/evolve/${evolve}/unit`);
-    // window.location.href = `unit`;
   }
-
-  // if (
-  //   props.onLearn.notes[0].title != "no" &&
-  //   props.onLearn.notes[0].title != Object
-  // ) {
-  //   evolve = "evolve_" + String(props.onLearn.notes[0].title);
-  //   num_evolve = props.onLearn.notes[0].title;
-  // }
-  // if (
-  //   props.onLearn.notes[1].title != "no" &&
-  //   props.onLearn.notes[1].title != Object
-  // ) {
-  //   unit = "unit_" + String(props.onLearn.notes[1].title);
-  //   num_unit = props.onLearn.notes[1].title;
-  // }
 
   const [step, setStep] = useState(0);
   const [flip, setFlip] = useState(false);
@@ -197,14 +50,6 @@ function CardsLearning(props) {
       setShowNextCard(true);
     }, 200);
   };
-
-  // if (
-  //   props.onLearn.notes.length > 3 &&
-  //   props.onLearn.notes[3].title != "no" &&
-  //   props.onLearn.notes[3].title != Object
-  // ) {
-  //   return <Navigate to="/Evolve" />;
-  // }
 
   return (
     <div>
